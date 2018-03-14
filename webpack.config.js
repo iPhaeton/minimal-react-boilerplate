d5666c069cbfddbf83623c8a41cf698a14dac1e3
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2018-03-14 19:37:51 +0300

    Install typescript

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,16 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: path.resolve(process.cwd(), 'app/app.js'),
+    entry: path.resolve(process.cwd(), 'app/app.ts'),
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
     output: {
         path: path.resolve(process.cwd(), 'dist'),
         filename: 'app.bundle.js'
