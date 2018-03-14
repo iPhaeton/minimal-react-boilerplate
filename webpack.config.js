059f57133169489d16f751a8d2454fce7c1ea90f
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2018-03-14 19:50:47 +0300

    Add index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -16,4 +17,9 @@ module.exports = {
         path: path.resolve(process.cwd(), 'dist'),
         filename: 'app.bundle.js'
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(process.cwd(), 'app/index.html'),
+        }),
+    ]
 };
